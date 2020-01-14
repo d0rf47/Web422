7b8eb291d63a2c4f91af2acaed5fee543252f417
d0rf47 <48887571+d0rf47@users.noreply.github.com>
2020-01-14 00:05:03 -0500

    origin

--- SalesAPI/server.js
+++ SalesAPI/server.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+ *  * WEB422 – Assignment 1
+ *  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ *  * No part of this assignment has been copied manually or electronically from any other source 
+ * * (including web sites) or distributed to other students. * 
+ * 
+ * * Name: Michael J Falconi Student ID: 143152189 Date: Jan 15 2020 
+ * * Heroku Link: _______________________________________________________________ 
+ * * ********************************************************************************/
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
